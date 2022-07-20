c62a86b3ddd9f368d26d8c5669dccc32a1594670
Marek2221 <dominikkucharczyk4@gmail.com>
2022-07-20 12:45:19 +0200

    Added aa image of school

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
                 <a href="/blog/create/post" class="btn btn-primary">Create a new Post</a>
             </div>
         </div><br>
+        <div class="separator"></div><br>
+        <img src="https://i0.wp.com/zs3-wyszkow.pl/wp-content/uploads/2020/12/szkolazs3-min.jpg?fit=1920%2C1200&ssl=1" style="height:250px; margin-left: 32%;" alt="Image of our school"><br><br>
         <div class="separator"></div>
     </div>
 
